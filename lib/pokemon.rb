329c409049e83cde8039f619add77ee2e040d0fe
Irene Scott <irenejinscott@gmail.com>
2020-06-12 05:09:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,12 +2,17 @@ class Pokemon
 
 attr_accessor :id, :name, :type, :db
 
+<<<<<<< HEAD
   def initialize(id:, name:, type:, db:)
+=======
+  def self.initialize(id, name, type, db)
+>>>>>>> 104dda3080f06c5c4524927ee6c0e2b5c7240a92
     @id = id
     @name = name
     @type = type
     @db = db
   end
+<<<<<<< HEAD
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
@@ -21,4 +26,6 @@ attr_accessor :id, :name, :type, :db
      Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db) #grab the first element index is 0
   end
 #each of these indexes represents a part of the hash which is each attribute for the pokemon
+=======
+>>>>>>> 104dda3080f06c5c4524927ee6c0e2b5c7240a92
 end
